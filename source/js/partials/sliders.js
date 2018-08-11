8d8d79b302a52b4075666cee6cbb748e570b015b
Константин Елагин <konstantinelagin@MacBook-Air-Konstantin.local>
2018-08-11 22:48:50 +0300

    ch

--- source/js/partials/sliders.js
+++ source/js/partials/sliders.js
@@ -1,10 +1,16 @@
 import Swiper from 'swiper/dist/js/swiper.js';
 let firstSlider = new Swiper('.first-slider', {
+  init: false,
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: '.first-slider__next',
+    prevEl: '.first-slider__prev',
   },
 });
 
-let secondSlider = new Swiper('.second-slider', {});
+let secondSlider = new Swiper('.second-slider', {
+  pagination: {
+    el: '.second-slider__pagination',
+    clickable: true
+  }
+});
 
